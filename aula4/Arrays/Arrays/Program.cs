01fa155ca27a5b0ee0a76391b4ce66e687669f2a
atalibaneto <atalibaneto@gmail.com>
2023-03-07 19:21:46 -0300

    aulas de C#

---
+++ aula4/Arrays/Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string> { "Marcos", "Victor", "Gabriela", "Gabriel", "Julia", "Pedro" };
+            names.Add("Ataliba");
+            Console.WriteLine(names[4]);
+            /*foreach (var name in names)
+            {
+                Console.WriteLine($"Lista de Nomes da TI32: {name.ToUpper()}!");
+            }*/
+            var r = new Random();
+            for (int i = 0; i < names.Count; i++)
+            {
+                r.Next(6);
+                Console.WriteLine(r);
+                Console.WriteLine($"Lista de Nomes da TI32: {names[i].ToLower()}!");
+            }
+
+            Console.ReadKey();
+        }
+        
+    }
+}
